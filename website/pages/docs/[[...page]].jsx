ca20708ff70fbe6f4648922da5e89da70236a4ff
Kevin Wang <kwangsan@gmail.com>
2021-12-06 19:24:22 -0500

    feat: versioned-docs

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -4,10 +4,7 @@ import PluginTierLabel from 'components/plugin-tier-label'
 import DevAlert from 'components/dev-alert'
 import Checklist from 'components/checklist'
 // Imports below are only used server-side
-import {
-  generateStaticPaths,
-  generateStaticProps,
-} from 'components/remote-plugin-docs/server'
+import { getStaticGenerationFunctions } from '@hashicorp/react-docs-page/server'
 
 //  Configure the docs path and remote plugin docs loading
 const additionalComponents = { PluginTierLabel, Checklist }
@@ -16,9 +13,9 @@ const localContentDir = 'content/docs'
 const mainBranch = 'master'
 const navDataFile = 'data/docs-nav-data.json'
 const product = { name: productName, slug: productSlug }
-const remotePluginsFile = 'data/docs-remote-plugins.json'
+// const remotePluginsFile = 'data/docs-remote-plugins.json'
 
-function DocsLayout({ isDevMissingRemotePlugins, ...props }) {
+export default function DocsLayout({ isDevMissingRemotePlugins, ...props }) {
   return (
     <>
       {isDevMissingRemotePlugins ? (
@@ -49,31 +46,30 @@ function DocsLayout({ isDevMissingRemotePlugins, ...props }) {
         baseRoute={baseRoute}
         product={product}
         staticProps={props}
+        showVersionSelect={process.env.ENABLE_VERSIONED_DOCS === 'true'}
       />
     </>
   )
 }
 
-export async function getStaticPaths() {
-  const paths = await generateStaticPaths({
-    localContentDir,
-    navDataFile,
-    remotePluginsFile,
-  })
-  return { paths, fallback: false }
-}
+const { getStaticPaths, getStaticProps } = getStaticGenerationFunctions(
+  process.env.ENABLE_VERSIONED_DOCS === 'true'
+    ? {
+        strategy: 'remote',
+        basePath: baseRoute,
+        fallback: 'blocking',
+        revalidate: 360, // 1 hour
+        product: productSlug,
+        mainBranch: mainBranch,
+      }
+    : {
+        strategy: 'fs',
+        localContentDir: localContentDir,
+        navDataFile: navDataFile,
+        product: productSlug,
+        revalidate: false,
+        mainBranch: mainBranch,
+      }
+)
 
-export async function getStaticProps({ params }) {
-  const props = await generateStaticProps({
-    additionalComponents,
-    localContentDir,
-    mainBranch,
-    navDataFile,
-    params,
-    product,
-    remotePluginsFile,
-  })
-  return { props }
-}
-
-export default DocsLayout
+export { getStaticPaths, getStaticProps }
